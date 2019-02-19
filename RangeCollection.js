50b0532b9746df49e91c6a4b36984a89be60d916
Vishnu Mishra <cybercognizant@gmail.com>
2019-02-19 21:58:33 +0530

    move the test cases from RangeCollection to inputs.js and test/RangeCollection.test.js

--- RangeCollection.js
+++ RangeCollection.js
@@ -31,45 +31,4 @@ class RangeCollection {
   }
 }
 
-// Example run
-const rc = new RangeCollection();
-
-rc.add([1, 5]);
-rc.print();
-// Should display: [1, 5)
-
-rc.add([10, 20]);
-rc.print();
-// Should display: [1, 5) [10, 20)
-
-rc.add([20, 20]);
-rc.print();
-// Should display: [1, 5) [10, 20)
-
-rc.add([20, 21]);
-rc.print();
-// Should display: [1, 5) [10, 21)
-
-rc.add([2, 4]);
-rc.print();
-// Should display: [1, 5) [10, 21)
-
-rc.add([3, 8]);
-rc.print();
-// Should display: [1, 8) [10, 21)
-
-rc.remove([10, 10]);
-rc.print();
-// Should display: [1, 8) [10, 21)
-
-rc.remove([10, 11]);
-rc.print();
-// Should display: [1, 8) [11, 21)
-
-rc.remove([15, 17]);
-rc.print();
-// Should display: [1, 8) [11, 15) [17, 21)
-
-rc.remove([3, 19]);
-rc.print();
-// Should display: [1, 3) [19, 21)
+//Moved the test into test/RangeCollection.test.js and also inputs.js
